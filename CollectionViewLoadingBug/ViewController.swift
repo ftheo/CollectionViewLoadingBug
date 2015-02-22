30d753a6dc6bbea1b13b459af88a8de67bed076b
Filippos Theodorakis <ftheodorakis@gmail.com>
2015-02-22 09:32:28 -0800

    Initial Commit

---
+++ CollectionViewLoadingBug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewLoadingBug
+//
+//  Created by Filippos Theodorakis on 2/22/15.
+//  Copyright (c) 2015 Ena LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
